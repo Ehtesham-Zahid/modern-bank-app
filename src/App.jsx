f7163e69a2c123cc97a5bc700cedc8a8f84b829f
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-12 17:39:58 +0500

    Testimonials Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary w-full overflow-hidden ">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
